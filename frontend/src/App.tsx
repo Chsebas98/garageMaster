f670eba16137913c87310290c4b51740f54f4b62
Sebastian <66321717+Chsebas98@users.noreply.github.com>
2023-05-30 09:48:10 -0500

    Garage Master front React-Ts --dependecias sweet alert, react-router-dom, bootstrapCDN

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+
+export default class App extends React.Component {
+  public render() {
+    return (
+      <div>
+        <h1>Hola</h1>
+      </div>
+    );
+  }
+}
