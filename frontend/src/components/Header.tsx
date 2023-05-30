1c49b7d3aed042eed90096049a71e2ba816f73c3
Robinson <robinjosellove@gmail.com>
2023-05-30 10:25:12 -0500

    landing page

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,32 @@
+import { Link } from 'react-router-dom';
+import '../styles/Header.css';
+
+export const Header = () => (
+  <header>
+    <nav className="navbar">
+      <a href="" className="navbar-logo">
+        Garage Master
+      </a>
+      <ul className="navbar-opt">
+        <li>
+          <a href="">Home</a>
+        </li>
+        <li>
+          <a href="">About</a>
+        </li>
+        <li>
+          <a href="">Cars</a>
+        </li>
+        <li>
+          <a href="">Services</a>
+        </li>
+        <li>
+          <a href="">Reviews</a>
+        </li>
+      </ul>
+      <Link to="/" className="navbar-login">
+        Login
+      </Link>
+    </nav>
+  </header>
+);
